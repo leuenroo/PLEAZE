714246782f40f95c0ecc318bc669b3e3d844044a
Will Leuenroth <leuenroo@gmail.com>
2021-07-06 21:48:27 -0400

    changing name of availability activity

--- app/src/main/java/com/leuenroo/pleaze/AvailabilityActivity.java
+++ app/src/main/java/com/leuenroo/pleaze/AvailabilityActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AvailabilityScreen extends AppCompatActivity {
+public class AvailabilityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_availability_screen);
+        setContentView(R.layout.activity_availability);
     }
 }
